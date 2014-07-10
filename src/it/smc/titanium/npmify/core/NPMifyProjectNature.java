38fac54c7206c90dc3c0dd83e71fc1fa21b258ee
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-07-10 10:49:40 +0200

    SF

--- src/it/smc/titanium/npmify/core/NPMifyProjectNature.java
+++ src/it/smc/titanium/npmify/core/NPMifyProjectNature.java
@@ -1,6 +1,7 @@
 package it.smc.titanium.npmify.core;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
 
